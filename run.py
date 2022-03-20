9527c97f6fddacfe5d8af113751cd20a7d4ba118
JessicaDJSilva <jessica.silvauk1@gmail.com>
2022-03-20 23:44:00 +0000

    add board game

--- run.py
+++ run.py
@@ -4,4 +4,11 @@ print("----------------\nWelcome to tic-tac-toe!\n----------------\n")
 print("You will play against of the computer, Good Luck 🙃")
 print("The player who succeeds in placing three of their marks in a\n horizontal, vertical, or diagonal row is the winner. ")
 
-
+print("Time to choose your move on the board\n this is the board")
+print("_ _ _")
+print("_ _ _")
+print("_ _ _")
+print("Select ONE number between 1 - 9,you can see the positions on this numeric board.")
+print("1 2 3")
+print("4 5 6")
+print("7 8 9")
