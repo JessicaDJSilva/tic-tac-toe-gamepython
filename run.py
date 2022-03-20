388f0f57873485dd9e1b8d533669af69a8e4aaca
JessicaDJSilva <jessica.silvauk1@gmail.com>
2022-03-20 23:28:18 +0000

    welcome messagens

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+print("----------------\nWelcome to tic-tac-toe!\n----------------\n")
+print("You will play against of the computer, Good Luck 🙃")
+print("The player who succeeds in placing three of their marks in a\n horizontal, vertical, or diagonal row is the winner. ")
+
+
