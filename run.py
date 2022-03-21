1b4048d6244d1748d31ce5495350085867599db4
JessicaDJSilva <jessica.silvauk1@gmail.com>
2022-03-21 14:10:31 +0000

    add fuction to slow down the game

--- run.py
+++ run.py
@@ -47,8 +47,11 @@ def who_goes_first():
         print("Better luck next time, computer goes first\n")
         return False
 
-
+# sleep funtion so the game do not run so fast
 def sleep_input(string):
+"""
+function to slow down the game.
+"""
     input_str = input(string)
     time.sleep(0.5)
     return input_str
@@ -68,14 +71,55 @@ def validate_and_input(string, valid_input):
 
 # ask for player move 
 def get_player_move():
+    """
+Function to aask the player move.
+    """
     return int(validate_and_input("""Choose your move 0,1,2
                  3,4,5
                  6,7,8\n""", ['0', '1', '2', '3', '4', '5', '6', '7', '8']))
 
 
+# mark player move on the board.
+
+def player_move(computer_board, player_board, player_move):
+    """
+    fuction to mark player move
+    """
+    if not computer_board & (1 << player_move) and not player_board & (
+            1 << player_move):
+        return player_board | (1 << player_move), True
+    else:
+        return None, False
+
+
+# function to define computer move 
+
+def computer_move(computer_moves, player_board, computer_board):
+    """
+ Define and mark move on the computer board.
+    """
+    print("computer is thinking...")
+    time.sleep(3)
+    while len(computer_moves) > 0:
+        computer_move = computer_moves.pop()
+        if not player_board & (1 << computer_move):
+            return computer_board | (1 << computer_move), computer_moves
+    return computer_board, computer_moves
+
+
+
+
+
+
+
+
+
+
+
+
 def main():
     print("----------------\nWelcome to tic-tac-toe!\n----------------\n")
-    print("You will play against of the computer, Good Luck 🙃\n")
+    print("You will play against of the computer, Good Luck 👍\n")
     print(
         "Rule: The player who succeeds in placing three of their marks in a\n horizontal, vertical, or diagonal row is the winner.\n"
     )
